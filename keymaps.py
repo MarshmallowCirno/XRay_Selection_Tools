2d6b16500e138889717ca7c94d3cdbf114dd9569
MarshmallowCirno <mindless.cirno@gmail.com>
2020-04-03 14:09:09 +0300

    Changed keymap draw order
    Reverse keymap item registration order to display in correct order in the keymap preferences

--- keymaps.py
+++ keymaps.py
@@ -9,42 +9,8 @@ def register_keymaps():
     if kc:
         km = kc.keymaps.new(name="Mesh", space_type='EMPTY')
 
-        kmi = km.keymap_items.new("mesh.select_box_xray", 'EVT_TWEAK_R', 'ANY')
-        kmi.properties.mode = 'SET'
-        kmi.properties.select_through = True
-        kmi.properties.select_all_faces = False
-        kmi.properties.select_all_edges = False
-        kmi.properties.show_xray = False
-        kmi.properties.wait_for_input = False
-        kmi.properties.hide_modifiers = True
-        kmi.active = False
-        addon_keymaps.append((km, kmi))
-                
-        kmi = km.keymap_items.new("mesh.select_box_xray", 'EVT_TWEAK_R', 'ANY', shift=True)
-        kmi.properties.mode = 'ADD'
-        kmi.properties.select_through = True
-        kmi.properties.select_all_faces = False
-        kmi.properties.select_all_edges = False
-        kmi.properties.show_xray = True
-        kmi.properties.wait_for_input = False
-        kmi.properties.hide_modifiers = True
-        kmi.active = False
-        addon_keymaps.append((km, kmi))
-        
-        kmi = km.keymap_items.new("mesh.select_box_xray", 'EVT_TWEAK_R', 'ANY', ctrl=True)
+        kmi = km.keymap_items.new("mesh.select_lasso_xray", 'EVT_TWEAK_L', 'ANY', ctrl=True, shift=True)
         kmi.properties.mode = 'SUB'
-        kmi.properties.select_through = True
-        kmi.properties.select_all_faces = False
-        kmi.properties.select_all_edges = False
-        kmi.properties.wait_for_input = False
-        kmi.properties.show_xray = True
-        kmi.properties.hide_modifiers = True
-        kmi.active = False
-        addon_keymaps.append((km, kmi))
-        
-        kmi = km.keymap_items.new("mesh.select_circle_xray", 'C', 'PRESS')
-        kmi.properties.mode = 'SET'
-        kmi.properties.wait_for_input = True
         kmi.properties.select_through = True
         kmi.properties.show_xray = True
         kmi.properties.hide_modifiers = True
@@ -58,24 +24,77 @@ def register_keymaps():
         kmi.properties.hide_modifiers = True
         kmi.active = False
         addon_keymaps.append((km, kmi))
-        
-        kmi = km.keymap_items.new("mesh.select_lasso_xray", 'EVT_TWEAK_L', 'ANY', ctrl=True, shift=True)
-        kmi.properties.mode = 'SUB'
+
+        kmi = km.keymap_items.new("mesh.select_circle_xray", 'C', 'PRESS')
+        kmi.properties.mode = 'SET'
+        kmi.properties.wait_for_input = True
         kmi.properties.select_through = True
         kmi.properties.show_xray = True
         kmi.properties.hide_modifiers = True
         kmi.active = False
         addon_keymaps.append((km, kmi))
-        
+
+        kmi = km.keymap_items.new("mesh.select_box_xray", 'EVT_TWEAK_R', 'ANY', ctrl=True)
+        kmi.properties.mode = 'SUB'
+        kmi.properties.select_through = True
+        kmi.properties.select_all_faces = False
+        kmi.properties.select_all_edges = False
+        kmi.properties.wait_for_input = False
+        kmi.properties.show_xray = True
+        kmi.properties.hide_modifiers = True
+        kmi.active = False
+        addon_keymaps.append((km, kmi))
+
+        kmi = km.keymap_items.new("mesh.select_box_xray", 'EVT_TWEAK_R', 'ANY', shift=True)
+        kmi.properties.mode = 'ADD'
+        kmi.properties.select_through = True
+        kmi.properties.select_all_faces = False
+        kmi.properties.select_all_edges = False
+        kmi.properties.show_xray = True
+        kmi.properties.wait_for_input = False
+        kmi.properties.hide_modifiers = True
+        kmi.active = False
+        addon_keymaps.append((km, kmi))
+
+        kmi = km.keymap_items.new("mesh.select_box_xray", 'EVT_TWEAK_R', 'ANY')
+        kmi.properties.mode = 'SET'
+        kmi.properties.select_through = True
+        kmi.properties.select_all_faces = False
+        kmi.properties.select_all_edges = False
+        kmi.properties.show_xray = False
+        kmi.properties.wait_for_input = False
+        kmi.properties.hide_modifiers = True
+        kmi.active = False
+        addon_keymaps.append((km, kmi))
+
         km = kc.keymaps.new(name="Object Mode", space_type='EMPTY')
 
-        kmi = km.keymap_items.new("object.select_box_xray", 'EVT_TWEAK_R', 'ANY')
+        kmi = km.keymap_items.new("object.select_lasso_xray", 'EVT_TWEAK_L', 'ANY', ctrl=True, shift=True)
+        kmi.properties.mode = 'SUB'
+        kmi.properties.show_xray = True
+        kmi.active = False
+        addon_keymaps.append((km, kmi))
+
+        kmi = km.keymap_items.new("object.select_lasso_xray", 'EVT_TWEAK_L', 'ANY', ctrl=True)
+        kmi.properties.mode = 'ADD'
+        kmi.properties.show_xray = True
+        kmi.active = False
+        addon_keymaps.append((km, kmi))
+
+        kmi = km.keymap_items.new("object.select_circle_xray", 'C', 'PRESS')
         kmi.properties.mode = 'SET'
+        kmi.properties.wait_for_input = True
+        kmi.properties.show_xray = True
+        kmi.active = False
+        addon_keymaps.append((km, kmi))
+
+        kmi = km.keymap_items.new("object.select_box_xray", 'EVT_TWEAK_R', 'ANY', ctrl=True)
+        kmi.properties.mode = 'SUB'
         kmi.properties.wait_for_input = False
         kmi.properties.show_xray = True
         kmi.active = False
         addon_keymaps.append((km, kmi))
-        
+
         kmi = km.keymap_items.new("object.select_box_xray", 'EVT_TWEAK_R', 'ANY', shift=True)
         kmi.properties.mode = 'ADD'
         kmi.properties.wait_for_input = False
@@ -83,32 +102,13 @@ def register_keymaps():
         kmi.active = False
         addon_keymaps.append((km, kmi))
         
-        kmi = km.keymap_items.new("object.select_box_xray", 'EVT_TWEAK_R', 'ANY', ctrl=True)
-        kmi.properties.mode = 'SUB'
+        kmi = km.keymap_items.new("object.select_box_xray", 'EVT_TWEAK_R', 'ANY')
+        kmi.properties.mode = 'SET'
         kmi.properties.wait_for_input = False
         kmi.properties.show_xray = True
         kmi.active = False
         addon_keymaps.append((km, kmi))
-        
-        kmi = km.keymap_items.new("object.select_circle_xray", 'C', 'PRESS')
-        kmi.properties.mode = 'SET'
-        kmi.properties.wait_for_input = True
-        kmi.properties.show_xray = True
-        kmi.active = False
-        addon_keymaps.append((km, kmi))
-        
-        kmi = km.keymap_items.new("object.select_lasso_xray", 'EVT_TWEAK_L', 'ANY', ctrl=True)
-        kmi.properties.mode = 'ADD'
-        kmi.properties.show_xray = True
-        kmi.active = False
-        addon_keymaps.append((km, kmi))
-        
-        kmi = km.keymap_items.new("object.select_lasso_xray", 'EVT_TWEAK_L', 'ANY', ctrl=True, shift=True)
-        kmi.properties.mode = 'SUB'
-        kmi.properties.show_xray = True
-        kmi.active = False
-        addon_keymaps.append((km, kmi))
-        
+
 
 
 def unregister_keymaps():
