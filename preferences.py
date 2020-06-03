a347fb37f9b5f4b02ffd9d3859c76541ad9e684e
MarshmallowCirno <mindless.cirno@gmail.com>
2020-06-03 17:14:40 +0300

    Add files via upload

---
+++ preferences.py
@@ -0,0 +1,11 @@
+import os
+import bpy
+
+
+def get_addon_name():
+    return os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+
+
+def get_preferences():
+    name = get_addon_name()
+    return bpy.context.preferences.addons[__package__].preferences
