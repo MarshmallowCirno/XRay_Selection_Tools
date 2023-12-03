492ebefaf3506bf0728820c53d3a3cca106e3da1
MarshmallowCirno <marshmallowcirno@gmail.com>
2023-12-03 19:16:15 +0300

    Version bump

--- space_view3d_xray_selection_tools/__init__.py
+++ space_view3d_xray_selection_tools/__init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "X-Ray Selection Tools",
     "author": "MarshmallowCirno",
-    "version": (4, 4, 0),
+    "version": (4, 5, 0),
     "blender": (3, 6, 0),
     "location": "Toolbar > Selection Tools",
     "description": "Box, lasso and circle selection tools with x-ray",
