bb6c78e53f936c6f5b396ea2de817f7816e6b9b7
MarshmallowCirno <marshmallowcirno@gmail.com>
2024-11-24 19:49:07 +0300

    Add type hinting for get_preferences()

--- space_view3d_xray_selection_tools/preferences.py
+++ space_view3d_xray_selection_tools/preferences.py
@@ -1,10 +1,16 @@
 import os
+from typing import TYPE_CHECKING
+
 import bpy
+
+if TYPE_CHECKING:
+    # Only imported for type-checking
+    from .ui_preferences import XRAYSELPreferences
 
 
 def get_addon_name():
     return os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
 
-def get_preferences():
+def get_preferences() -> "XRAYSELPreferences":
     return bpy.context.preferences.addons[__package__].preferences
