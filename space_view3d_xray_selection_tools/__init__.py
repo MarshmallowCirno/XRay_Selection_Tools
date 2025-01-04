89e4fdb9c5c281de796e4ba320964d74e7aba5ad
MarshmallowCirno <marshmallowcirno@gmail.com>
2025-01-04 17:09:41 +0300

    refactor(reloading): automate submodule reloading mechanism
    - Replaced the manual import and reload logic for submodules with
    an automated function.
    - The new function accepts a package as input and dynamically
    identifies and reloads all submodules.
    - Simplifies code maintenance and eliminates the need for a hardcoded
    list of submodules.

--- space_view3d_xray_selection_tools/__init__.py
+++ space_view3d_xray_selection_tools/__init__.py
@@ -15,8 +15,6 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # ##### END GPL LICENSE BLOCK #####
-
-
 bl_info = {
     "name": "X-Ray Selection Tools",
     "author": "MarshmallowCirno",
@@ -30,108 +28,30 @@ bl_info = {
     "tracker_url": "https://blenderartists.org/t/x-ray-selection-tools/1212316",
 }
 
-
-RELOADABLE_MODULES = (
-    "types",
-    "edge_attr",
-    "loop_attr",
-    "poly_attr",
-    "vert_attr",
-    "view3d",
-    "timer",
-    "geometry_tests",
-    "selection_utils",
-    "mesh_intersect",
-    "object_intersect",
-    "object_intersect_box",
-    "object_intersect_circle",
-    "object_intersect_lasso",
-    "object_intersect_shared",
-    "mesh_modal",
-    "object_modal",
-    "mesh_ot_box",
-    "mesh_ot_circle",
-    "mesh_ot_lasso",
-    "mesh_ot_toggle",
-    "object_ot_box",
-    "object_ot_circle",
-    "object_ot_lasso",
-    "help",
-    "ot_keymap",
-    "tools",
-    "tools_main",
-    "tools_dummy",
-    "tools_keymap",
-    "tools_utils",
-    "keymaps_props",
-    "keymap_ui",
-    "mesh_tools_ui",
-    "object_tools_ui",
-    "draw",
-    "keymaps_props",
-    "tools_props",
-    "properties",
-    "addon_preferences",
-    "startup_handlers",
-)
-
-# when bpy is already in local, we know this is not the initial import...
+# Support reloading submodules
 if "bpy" in locals():
-    # ...so we need to reload our submodule(s) using importlib
-    import importlib
+    import sys
 
-    for module in RELOADABLE_MODULES:
-        if module in locals():
-            importlib.reload(locals()[module])
+    from . import reloader
+
+    reloader.reload_package_modules(sys.modules[__name__])
+
+
+import bpy
+
+if not bpy.app.background:
+    from . import operators, preferences, startup_handlers, tools
+
+    def register():
+        preferences.register()
+        operators.register()
+        tools.register()
+        startup_handlers.register()
+
+    def unregister():
+        preferences.unregister()
+        operators.unregister()
+        tools.unregister()
+        startup_handlers.unregister()
 else:
-    import bpy
-
-    if not bpy.app.background:  # Prevent imports when run in background
-        from .functions.mesh_attr import edge_attr, loop_attr, poly_attr, vert_attr
-        from .functions import (
-            geometry_tests,
-            timer,
-            view3d,
-        )
-        from .functions.intersections import selection_utils
-        from .functions.intersections.object_intersect import (
-            object_intersect_box,
-            object_intersect_circle,
-            object_intersect_lasso,
-            object_intersect_shared,
-        )
-        from .functions.intersections import mesh_intersect, object_intersect
-        from .functions.modals import mesh_modal, object_modal
-        from .operators.mesh_ot import mesh_ot_box, mesh_ot_circle, mesh_ot_lasso, mesh_ot_toggle
-        from .operators.object_ot import object_ot_box, object_ot_circle, object_ot_lasso
-        from .operators import help_ot, ot_keymap
-        from .preferences.draw import keymap_ui, mesh_tools_ui, object_tools_ui
-        from .preferences.properties import keymaps_props, tools_props
-        from .preferences import draw, properties, addon_preferences
-        from . import types, tools, startup_handlers, preferences
-        from .tools import tools_main, tools_dummy, tools_keymap, tools_utils
-
-
-import bpy  # noqa
-
-
-def register():
-
-    if bpy.app.background:
-        return
-
-    preferences.register()
-    operators.register()
-    tools.register()
-    startup_handlers.register()
-
-
-def unregister():
-
-    if bpy.app.background:
-        return
-
-    preferences.unregister()
-    operators.unregister()
-    tools.unregister()
-    startup_handlers.unregister()
+    register = unregister = lambda: None
---
+++ space_view3d_xray_selection_tools/reloader.py
@@ -0,0 +1,46 @@
+import importlib
+import pathlib
+import types
+from typing import Iterator
+
+
+def _module_children(module: types.ModuleType, package_dir: pathlib.Path) -> Iterator[types.ModuleType]:
+    """Yields child modules of parent module within the specified package directory."""
+    for attr in vars(module).values():
+        if not isinstance(attr, types.ModuleType):
+            continue
+        if not (filename := getattr(attr, "__file__", None)):
+            continue
+        if package_dir not in pathlib.Path(filename).parents:
+            continue
+        yield attr
+
+
+def _package_modules(package: types.ModuleType) -> list[types.ModuleType]:
+    """Returns a list of all modules within a package, including submodules."""
+    package_dir = pathlib.Path(package.__file__).parent
+    modules = []
+    stack = [(package, _module_children(package, package_dir))]
+    visited = set()
+
+    # Postorder traversal
+    while stack:
+        current_module, children = stack[-1]
+        try:
+            child = next(children)
+            if child.__file__ in visited:
+                continue
+            visited.add(child.__file__)
+            stack.append((child, _module_children(child, package_dir)))
+        except StopIteration:
+            modules.append(current_module)
+            stack.pop()
+
+    return modules
+
+
+def reload_package_modules(package: types.ModuleType) -> None:
+    """Reloads all modules within a package."""
+    assert hasattr(package, "__package__")
+    for module in _package_modules(package):
+        importlib.reload(module)
