5234b995b116c97fa56a4fa47acb51691deed67d
MarshmallowCirno <marshmallowcirno@gmail.com>
2025-02-23 13:12:26 +0300

    fix(reloading): prevent imports when blender run in the background

--- space_view3d_xray_selection_tools/__init__.py
+++ space_view3d_xray_selection_tools/__init__.py
@@ -84,31 +84,35 @@ if "bpy" in locals():
         if module_name in locals():
             importlib.reload(locals()[module_name])
 else:
-    from . import addon_info, types  # noqa
-    from .icon import lasso_cursor
-    from .operators import ot_keymap, xraysel_ot_info
-    from .functions import geometry_tests, timer, view3d_utils
-    from .functions.mesh_attr import edge_attr, loop_attr, poly_attr, vert_attr
-    from .functions.intersections import selection_utils, mesh_intersect, object_intersect
-    from .functions.modals import mesh_modal
-    from .operators.mesh_ot import mesh_ot_box, mesh_ot_circle, mesh_ot_lasso, mesh_ot_toggle
-    from .functions.intersections.object_intersect import (
-        object_intersect_shared,
-        object_intersect_box,
-        object_intersect_circle,
-        object_intersect_lasso,
-    )
-    from .functions.intersections import object_intersect
-    from .functions.modals import object_modal
-    from .operators.object_ot import object_ot_box, object_ot_circle, object_ot_lasso
-    from . import operators
-    from .tools import tools_keymap, tools_utils, tools_dummy, tools_main
-    from . import tools
-    from .preferences.draw import keymap_ui, mesh_tools_ui, object_tools_ui
-    from .preferences import draw
-    from .preferences.properties import keymaps_props, tools_props
-    from .preferences import addon_preferences
-    from . import preferences, startup_handlers
+    import bpy
+
+    # Prevent imports when run in the background, since gpu shaders will not be available
+    if not bpy.app.background:
+        from . import addon_info, types  # noqa
+        from .icon import lasso_cursor
+        from .operators import ot_keymap, xraysel_ot_info
+        from .functions import geometry_tests, timer, view3d_utils
+        from .functions.mesh_attr import edge_attr, loop_attr, poly_attr, vert_attr
+        from .functions.intersections import selection_utils, mesh_intersect, object_intersect
+        from .functions.modals import mesh_modal
+        from .operators.mesh_ot import mesh_ot_box, mesh_ot_circle, mesh_ot_lasso, mesh_ot_toggle
+        from .functions.intersections.object_intersect import (
+            object_intersect_shared,
+            object_intersect_box,
+            object_intersect_circle,
+            object_intersect_lasso,
+        )
+        from .functions.intersections import object_intersect
+        from .functions.modals import object_modal
+        from .operators.object_ot import object_ot_box, object_ot_circle, object_ot_lasso
+        from . import operators
+        from .tools import tools_keymap, tools_utils, tools_dummy, tools_main
+        from . import tools
+        from .preferences.draw import keymap_ui, mesh_tools_ui, object_tools_ui
+        from .preferences import draw
+        from .preferences.properties import keymaps_props, tools_props
+        from .preferences import addon_preferences
+        from . import preferences, startup_handlers
 
 import bpy
 
