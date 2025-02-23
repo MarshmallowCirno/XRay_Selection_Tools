2612187d5d59ce68b4bdafd191e552153b52533b
MarshmallowCirno <marshmallowcirno@gmail.com>
2025-02-23 15:32:16 +0300

    refactor(startup): remove depsgraph handler, load_post is sufficient

--- space_view3d_xray_selection_tools/startup_handlers.py
+++ space_view3d_xray_selection_tools/startup_handlers.py
@@ -26,28 +26,16 @@ def _activate_tool():
                         area.tag_redraw()
 
 
-def _activate_tool_on_startup(_scene: bpy.types.Scene, _depsgraph: bpy.types.Depsgraph) -> None:
-    if _activate_tool_on_startup in bpy.app.handlers.depsgraph_update_post:
-        bpy.app.handlers.depsgraph_update_post.remove(_activate_tool_on_startup)
-    _activate_tool()
-
-
 @bpy.app.handlers.persistent
 def _activate_tool_on_file_load(_scene: bpy.types.Scene) -> None:
     _activate_tool()
 
 
 def register():
-    if not _activate_tool_on_startup in bpy.app.handlers.depsgraph_update_post:
-        bpy.app.handlers.depsgraph_update_post.append(_activate_tool_on_startup)
-
     if not _activate_tool_on_file_load in bpy.app.handlers.load_post:
         bpy.app.handlers.load_post.append(_activate_tool_on_file_load)
 
 
 def unregister():
-    if _activate_tool_on_startup in bpy.app.handlers.depsgraph_update_post:
-        bpy.app.handlers.depsgraph_update_post.remove(_activate_tool_on_startup)
-
     if _activate_tool_on_file_load in bpy.app.handlers.load_post:
         bpy.app.handlers.load_post.remove(_activate_tool_on_file_load)
