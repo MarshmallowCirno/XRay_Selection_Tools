64d62e07541af779227099b4b1ef9be961a1ba3f
MarshmallowCirno <marshmallowcirno@gmail.com>
2026-05-05 17:12:27 +0300

    fix(operators): fix property param type

--- space_view3d_xray_selection_tools/preferences/properties/tools_props.py
+++ space_view3d_xray_selection_tools/preferences/properties/tools_props.py
@@ -108,7 +108,7 @@ class XRAYSELToolMeDirectionProps(bpy.types.PropertyGroup):
         select_backfacing: bpy.props.BoolProperty(
             name="Select Backfacing",
             description=(
-                "Select elements with normals pointing away from the view. Works only in \"Select Through\" mode",
+                "Select elements with normals pointing away from the view. Works only in \"Select Through\" mode"
             ),
             default=True,
         )
